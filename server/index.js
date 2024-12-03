7ec3f1758bc001f26b16a8ebfd68aaed6812f7c9
joan <joancallanaupalaurente@gmail.com>
2024-12-03 11:03:30 -0500

    sdfs

--- server/index.js
+++ server/index.js
@@ -59,5 +59,5 @@ conectarPlatosSocket(io);
 
 const PORT = process.env.PORT || 4000;
 server.listen(PORT, () => {
-  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+  console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
