0688b3d28f2d8d3083dffc4a6c5e218d0a4f6ade
TuNombreDeUsuario <TuCorreo@dominio.com>
2025-02-05 10:29:32 -0500

    dsfs8wer

--- server/routes/bookmarksRoutes.js
+++ server/routes/bookmarksRoutes.js
@@ -9,7 +9,7 @@ const router = express.Router();
 
 // Rutas de los marcadores
 router.post("/bookmarks/guardar", createBookmark);
-router.put("./bookmarks/update/:id", updateBookmark);
+router.put("/bookmarks/update/:id", updateBookmark);
 router.get("/bookmarks/obtener", getAllBookmarks);
 router.delete("/bookmarks/eliminar/:id", deleteBookmark);
 
