222225a78dfc3ca293eac854892e4a817c604f66
TuNombreDeUsuario <TuCorreo@dominio.com>
2025-02-05 10:03:15 -0500

    dfsfsf

--- server/models/Bookmarks.js
+++ server/models/Bookmarks.js
@@ -1,26 +1,28 @@
-import { DataTypes } from 'sequelize';
+import { DataTypes } from "sequelize";
 import sequelize from "../config/db.js";
 
 // Definir el modelo de Bookmark
-const Bookmarks = sequelize.define('Bookmark', {
-    url: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    titulo: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    imagen: {
-      type: DataTypes.TEXT,  // Aquí usamos TEXT para almacenar la imagen en Base64
-      allowNull: false
-    },
-    tags: {
-      type: DataTypes.ARRAY(DataTypes.STRING),
-      allowNull: true
+const Bookmarks = sequelize.define("Bookmark", {
+  url: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  titulo: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  imagen: {
+    type: DataTypes.TEXT, // Aquí usamos TEXT para almacenar la imagen en Base64
+    allowNull: false,
+  },
+  tags: {
+    type: DataTypes.ARRAY(DataTypes.STRING),
+    allowNull: true,
+  },
+  nota : {
+    type: DataTypes.TEXT,
+    allowNull: true,
   }
-  
-
-  });
+});
 
 export default Bookmarks;
