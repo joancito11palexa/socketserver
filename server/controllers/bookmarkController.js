276fb254f885b7a9c02d4b9b4d253cf576a02568
TuNombreDeUsuario <TuCorreo@dominio.com>
2025-02-05 10:12:51 -0500

    dfsfsdfsf

--- server/controllers/bookmarkController.js
+++ server/controllers/bookmarkController.js
@@ -2,18 +2,48 @@ import Bookmark from "../models/Bookmarks.js";
 
 // Crear un nuevo marcador
 export const createBookmark = async (req, res) => {
-  const { url, titulo, imagen } = req.body;
+  const { url, titulo, imagen, tags, nota } = req.body; // Aseguramos que se pueden incluir 'tags' y 'nota'
+
   try {
-    const nuevoBookmark = await Bookmark.create({ url, titulo, imagen });
+    const nuevoBookmark = await Bookmark.create({
+      url,
+      titulo,
+      imagen,
+      tags,
+      nota,
+    });
+
     res.status(201).json({
       mensaje: "Marcador creado exitosamente",
       data: nuevoBookmark,
     });
   } catch (error) {
-    console.error(error);
+    console.error("Error al crear el marcador:", error);
     res.status(500).json({ error: "Error al crear el marcador" });
   }
 };
+export const updateBookmark = async (req, res) => {
+  const { id } = req.params;
+  const { url, titulo, imagen, tags, nota } = req.body;
+
+  try {
+    const bookmark = await Bookmark.findByPk(id);
+    if (!bookmark) {
+      return res.status(404).json({ error: "Marcador no encontrado" });
+    }
+
+    await bookmark.update({ url, titulo, imagen, tags, nota });
+
+    res.status(200).json({
+      mensaje: "Marcador actualizado exitosamente",
+      data: bookmark,
+    });
+  } catch (error) {
+    console.error("Error al actualizar el marcador:", error);
+    res.status(500).json({ error: "Error al actualizar el marcador" });
+  }
+};
+
 
 // Obtener todos los marcadores
 export const getAllBookmarks = async (req, res) => {
--- server/routes/bookmarksRoutes.js
+++ server/routes/bookmarksRoutes.js
@@ -3,11 +3,13 @@ import {
   createBookmark,
   getAllBookmarks,
   deleteBookmark,
+  updateBookmark,
 } from "../controllers/bookmarkController.js";
 const router = express.Router();
 
 // Rutas de los marcadores
 router.post("/bookmarks/guardar", createBookmark);
+router.put("./bookmarks/update/:id", updateBookmark);
 router.get("/bookmarks/obtener", getAllBookmarks);
 router.delete("/bookmarks/eliminar/:id", deleteBookmark);
 
